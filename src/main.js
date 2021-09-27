ea1e083abf6ed972643c0d1267e0f9c75832c1b9
CarolineNakazato <carolinenakazato@yahoo.com.br>
2021-09-27 17:18:15 -0300

    Add files via upload
    trocando firebase

--- src/main.js
+++ src/main.js
@@ -4,13 +4,14 @@ import router from "./router";
 import firebase from "firebase";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBOF8LFvRQ__Z1QVdTENEBR_303oyABbF4",
-    authDomain: "vue-firebase-tutorial-1b911.firebaseapp.com",
-    projectId: "vue-firebase-tutorial-1b911",
-    storageBucket: "vue-firebase-tutorial-1b911.appspot.com",
-    messagingSenderId: "252575828557",
-    appId: "1:252575828557:web:f4e1a1244e1d0c31fa6a04"
-};
+    apiKey: "AIzaSyBHn0tC1gshzbCCnZ2RskTqixakCHCcNGc",
+    authDomain: "comunica-d93af.firebaseapp.com",
+    projectId: "comunica-d93af",
+    storageBucket: "comunica-d93af.appspot.com",
+    messagingSenderId: "586210287956",
+    appId: "1:586210287956:web:7847c922f1f6f3b42da8c1",
+    measurementId: "G-V9PJMY1CHH"
+  };
 
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
